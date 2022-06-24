b5d8120466896a9fab2de2e7fff093e4cd899f65
Kirill Ki <kirillkalapov@gmail.com>
2022-06-24 11:05:11 +0200

    Initial Commit

---
+++ IamKirill/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IamKirill
+//
+//  Created by  Mr.Ki on 24.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IamKirill/IamKirillApp.swift
@@ -0,0 +1,17 @@
+//
+//  IamKirillApp.swift
+//  IamKirill
+//
+//  Created by  Mr.Ki on 24.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct IamKirillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
